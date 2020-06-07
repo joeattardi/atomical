7e8ee49b0c266c6c9689c314eaa81f073a372e49
Joe Attardi <jattardi@gmail.com>
2020-06-06 22:48:19 -0400

    Add very basic calendar

--- src/index.js
+++ src/index.js
@@ -6,6 +6,54 @@ import calendarTemplate from './templates/calendar.mustache';
 
 console.log(styles);
 
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+const dayLabels = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+]
+
 export default function atomical(el) {
-  el.innerHTML = render(calendarTemplate, { name: 'Joe', classes: styles });
+  const now = new Date();
+  const firstDay = new Date(now.getFullYear(), now.getMonth(), 1);
+  const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+  const daysInMonth = lastDay.getDate();
+  const firstWeekday = firstDay.getDay();
+  
+  const placeholders = [];
+  placeholders.length = firstWeekday;
+
+  const days = [];
+  for (let i = 1; i <= daysInMonth; i++) {
+    days.push(i);
+  }
+
+  el.innerHTML = render(calendarTemplate, {
+    classes: styles,
+    month: months[now.getMonth()],
+    year: now.getFullYear(),
+    date: now,
+    dayLabels,
+    days,
+    daysInMonth,
+    placeholders
+  });
 }
