0319745310ca560b343fec7ccaffd2e0a4c42a3e
Joe Attardi <jattardi@gmail.com>
2020-09-11 20:35:49 -0400

    Update site

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import Prism from 'prismjs';
 import 'prismjs/themes/prism.css';
 
@@ -17,14 +17,14 @@ export default function Home() {
     <div>
       <div className={styles.hero}>
         <img src={hero} className={styles.heroImage} />
-        <h2>JavaScript Calendar Component</h2>
+        <h2>Vanilla JavaScript Calendar Component</h2>
         <div className={styles.badges}>
           <a href="https://npmjs.com/package/atomical"><img src="https://img.shields.io/npm/v/atomical" /></a>
           <a href="https://github.com/joeattardi/atomical"><img src="https://img.shields.io/github/stars/joeattardi/atomical?style=social" /></a>
         </div>
       </div>
       <div className={styles.header}>
-        <code className={styles.installationCode}>npm install --save atomical</code>
+        <code className={styles.installationCode}>npm install atomical</code>
       </div>
 
       <div className={styles.main}>
@@ -32,15 +32,11 @@ export default function Home() {
         <div className={styles.codeExample}>
           <h3>Basic usage</h3>
           <p>
-            Just call the <code>atomical</code> function on a container element, and a calendar 
-            of the current month will be rendered.
+            Import the <code>atomical</code> module somewhere in your site or app, then you can create calendars in your HTML markup by using the <code>&lt;atomical-calendar&gt;</code> custom element.
           </p>
           <pre>
-            <code className="language-javascript">
-              {`import atomical from 'atomical';
-
-const container = document.querySelector('#calendar-container');
-atomical(container);`}
+            <code className="language-html">
+              {`<atomical-calendar></atomical-calendar>`}
             </code>
           </pre>
         </div>
@@ -51,20 +47,17 @@ atomical(container);`}
         <div className={styles.codeExample}>
           <h3>Customize</h3>
           <p>
-            There are several customization options available.
+            You can customize the month shown with attributes, as well as override colors by defining CSS variables.
           </p>
           <pre>
-            <code className="language-javascript">
-              {`import atomical from 'atomical';
-
-const container = document.querySelector('#calendar-container');
-atomical(container, {
-  month: 0,
-  year: 2021,
-  colors: {
-    headerBackground: 'blue'
+            <code className="language-html">
+              {`<style>
+  atomical-calendar {
+    --header-background: blue;
   }
-});`}
+</style>
+
+<atomical-calendar month="0" year="2021"></atomical-calendar>`}
             </code>
           </pre>
         </div>
